c8f6502960beaca4785890295be7d0270cee4ea5
Ilya Savitsky <ilya.savitsky@codethink.co.uk>
2025-02-02 17:55:55 +0000

    Allow to assemble the website on every arch
    Signed-off-by: Ilya Savitsky <ilya.savitsky@codethink.co.uk>

--- flake.nix
+++ flake.nix
@@ -2,23 +2,33 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { self, nixpkgs }:
-    let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
-    in
     {
-
-      packages.x86_64-linux.savitsky-dev = pkgs.stdenv.mkDerivation {
-        name = "savitsky.dev";
-        src = ./.;
-        installPhase = ''
-          mkdir -p $out/static
-          cp index.html $out/static
-        '';
-      };
-    };
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages = rec {
+          default = savitsky-dev;
+          savitsky-dev = pkgs.stdenv.mkDerivation {
+            name = "savitsky.dev";
+            src = ./.;
+            installPhase = ''
+              mkdir -p $out/static
+              cp index.html $out/static
+            '';
+          };
+        };
+      }
+    );
 }
